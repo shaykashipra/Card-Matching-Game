8cf4c85074956c6721899abab8d23fab05197f1a
shaykashipra2 <triviatruism@gmail.com>
2024-01-27 19:20:00 +0600

    file created and uploaded

---
+++ game.js
@@ -0,0 +1,110 @@
+let username;
+
+
+
+
+const cards = document.querySelectorAll(".card");
+const timeTag = document.querySelector(".time b");
+const flipsTag = document.querySelector(".flips b");
+const refreshBtn = document.querySelector(".details button");
+const winnerMessage = document.querySelector(".winner");
+
+let maxTime = 30;
+let timeLeft = maxTime;
+let flips = 0;
+let matched = 0;
+let disableDeck = false;
+let isPlaying = false;
+let cardOne, cardTwo, timer;
+
+function initTimer() {
+  if (timeLeft <= 0) {
+      setTimeout(() => {
+        winnerMessage.textContent = username + " is a looser!";
+      }, 500);
+    
+    return clearInterval(timer);
+  
+  }
+  timeLeft--;
+  timeTag.innerText = timeLeft;
+}
+
+function flipCard({ target: clickedCard }) {
+
+    if (!isPlaying) {
+      isPlaying = true;
+      timer = setInterval(initTimer, 1000);
+    }
+
+
+
+  if (cardOne !== clickedCard && !disableDeck  && timeLeft > 0) {
+    
+    flips++;
+    flipsTag.innerText = flips;
+
+
+    clickedCard.classList.add("flip");
+    if (!cardOne) {
+      return (cardOne = clickedCard);
+    }
+    cardTwo = clickedCard;
+    disableDeck = true;
+    let cardOneImg = cardOne.querySelector(".back-view img").src,
+      cardTwoImg = cardTwo.querySelector(".back-view img").src;
+    matchCards(cardOneImg, cardTwoImg);
+  }
+}
+
+function matchCards(img1, img2) {
+  if (img1 === img2) {
+    matched++;
+   if (matched == 8 && timeLeft > 0) {
+     setTimeout(()=>{
+     winnerMessage.textContent = username+" is a Winner";},500);
+     return clearInterval(timer);
+
+   }
+    cardOne.removeEventListener("click", flipCard);
+    cardTwo.removeEventListener("click", flipCard);
+    cardOne = cardTwo = "";
+    return (disableDeck = false);
+  }
+  setTimeout(() => {
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+  }, 400);
+
+  setTimeout(() => {
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = "";
+    disableDeck = false;
+  }, 1200);
+}
+
+function shuffleCard() {
+  username = window.prompt("what is your name?");
+  winnerMessage.textContent = "";
+  matched = 0;
+  disableDeck = false;
+  cardOne = cardTwo = "";
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+  arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+  cards.forEach((card, i) => {
+    card.classList.remove("flip");
+    let imgTag = card.querySelector(".back-view img");
+    imgTag.src = `images/img-${arr[i]}.png`;
+    card.addEventListener("click", flipCard);
+  });
+}
+
+shuffleCard();
+
+refreshBtn.addEventListener("click", shuffleCard);
+
+
+cards.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
